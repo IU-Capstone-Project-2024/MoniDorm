134aa3d8084c36b6659a40b00861f50269ae3509
Daniil Prostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-06-19 23:13:05 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 function App() {
 	
 	return (
-		<div className="App">\
+		<div className="App">
 			<iframe src="http://10.90.138.215:3000/d-solo/fdpa1canapou8a/monidorm-dashboard?orgId=1&from=1718219692383&to=1718824492383&panelId=1" width="650" height="400" frameborder="0"></iframe>
 		</div>
 	);
