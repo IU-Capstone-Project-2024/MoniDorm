0f5746eb0ac3055d80ff87ae585dbe3543932532
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-06-19 23:10:13 +0300

    react init with grafana

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+function App() {
+	
+	return (
+		<div className="App">\
+			<iframe src="http://10.90.138.215:3000/d-solo/fdpa1canapou8a/monidorm-dashboard?orgId=1&from=1718219692383&to=1718824492383&panelId=1" width="650" height="400" frameborder="0"></iframe>
+		</div>
+	);
+}
+
+export default App;
