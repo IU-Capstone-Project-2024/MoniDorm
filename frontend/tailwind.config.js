4e1ac80b1eccd3c914b5067e1ba58b84f698cc01
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-06-21 22:44:51 +0300

    Tailwind and daisyUI integration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+		themes: ['light', 'dark']
+	}
+}
