a7696445feefee0b728c80f567b7340aab925ce4
m0t9 <matveykorinenko@gmail.com>
2024-06-22 18:06:03 +0300

    fix(bot): update state only after sending a message

--- telegram-bot/handling/handlers.py
+++ telegram-bot/handling/handlers.py
@@ -34,6 +34,11 @@ async def auth_email_process(msg: Message, state: FSMContext, mail_client: Clien
     if regexp.match(user_email):
         code = str(random.randint(100000, 999999))
         mail_client.send_authentication_code(user_email, code)
+        await msg.answer(f'Confirmation code was sent to `{user_email}`. '
+                         f'In order to finish authorization, send it here.'
+                         f'\n\nIf you figured out that email address contains mistake, '
+                         f'press the button below.',
+                         reply_markup=all_callbacks.get_email_kb(), parse_mode="Markdown")
         await state.update_data({
             "email": user_email,
             "expected_code": code,
@@ -41,11 +46,6 @@ async def auth_email_process(msg: Message, state: FSMContext, mail_client: Clien
                                datetime.timedelta(minutes=float(os.getenv("EMAIL_CODE_EXPIRATION_MINS")))
         })
         await state.set_state(DialogStates.AuthorizationConfirmationAwaiting)
-        await msg.answer(f'Confirmation code was sent to `{user_email}`. '
-                         f'In order to finish authorization, send it here.'
-                         f'\n\nIf you figured out that email address contains mistake, '
-                         f'press the button below.',
-                         reply_markup=all_callbacks.get_email_kb(), parse_mode="Markdown")
 
     else:
         await msg.answer('Sorry, seems like it is not a correct Innopolis University '
@@ -55,9 +55,9 @@ async def auth_email_process(msg: Message, state: FSMContext, mail_client: Clien
 @router.callback_query(StateFilter(DialogStates.AuthorizationConfirmationAwaiting),
                        all_callbacks.EmailCallback.filter())
 async def auth_email_revert(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.answer(text='Please, enter your Innopolis University email again in order to authorize.')
     await state.set_data({})
     await state.set_state(DialogStates.AuthorizationAwaiting)
-    await callback.message.answer(text='Please, enter your Innopolis University email again in order to authorize.')
 
 
 @router.message(StateFilter(DialogStates.AuthorizationConfirmationAwaiting))
@@ -67,6 +67,12 @@ async def auth_code_process(message: Message, state: FSMContext, mail_client: Cl
     user_email = (await state.get_data())["email"]
 
     if datetime.datetime.now() > expiration_time:
+        await message.answer(
+            f"Your authorization code has expired\n\n"
+            f"New one was sent to previously `{user_email}`",
+            reply_markup=all_callbacks.get_email_kb(),
+            parse_mode="Markdown"
+        )
         code = str(random.randint(100000, 999999))
         await state.update_data(
             code_expiration=datetime.datetime.now() +
@@ -74,16 +80,10 @@ async def auth_code_process(message: Message, state: FSMContext, mail_client: Cl
             expected_code=code
         )
         mail_client.send_authentication_code(user_email, code)
-        await message.answer(
-            f"Your authorization code has expired\n\n"
-            f"New one was sent to previously `{user_email}`",
-            reply_markup=all_callbacks.get_email_kb(),
-            parse_mode="Markdown"
-        )
     elif expected_code == message.text:
+        await message.answer("You are authorized now! Enjoy the usage of monidorm!")
         await state.set_state(DialogStates.Authorized)
         await state.update_data(expected_code="")
-        await message.answer("You are authorized now! Enjoy the usage of monidorm!")
     else:
         await message.answer("The code seems to be incorrect. Please, try again or reset the email.")
 
@@ -95,8 +95,8 @@ async def failure_report_init(
         state: FSMContext
 ):
     initial_transition = report_provider.get_callback(1)
-    await state.set_state(DialogStates.Reporting)
     await msg.answer('Report a failure!', reply_markup=initial_transition.keyboard())
+    await state.set_state(DialogStates.Reporting)
 
 
 @router.message(
@@ -107,8 +107,8 @@ async def failure_report_init(
     ), Command("menu")
 )
 async def interrupt_and_go_to_menu(msg: Message, state: FSMContext):
-    await state.set_state(DialogStates.Authorized)
     await msg.answer('Everything is interrupted, you are at the main menu now.')
+    await state.set_state(DialogStates.Authorized)
 
 
 @router.callback_query(StateFilter(DialogStates.Reporting), report_callbacks.ReportingKbCallback.filter())
@@ -121,21 +121,20 @@ async def report_processing(
     go_to = callback_data.window_id
     callback = report_provider.get_callback(go_to)
     if isinstance(callback, CancelCallback):
-        await state.set_state(DialogStates.Authorized)
         await query.message.delete()
+        await state.set_state(DialogStates.Authorized)
     elif isinstance(callback, CategoryCallback):
-        await state.update_data(report={
-            'placement': callback.placements(),
-            'category': callback.category()
-        })
-        await state.set_state(DialogStates.ReportCommentAwaiting)
         await query.message.edit_text(
-            'Thank you for the assistance!\n\n'
             'If you would like to provide some commentaries on failure — '
             'please, send a message with them, otherwise press the button below\n\n'
             'If you would like go back — press the corresponding button',
             reply_markup=all_callbacks.get_detailed_report_kb(callback.parent_id())
         )
+        await state.update_data(report={
+            'placement': callback.placements(),
+            'category': callback.category()
+        })
+        await state.set_state(DialogStates.ReportCommentAwaiting)
     elif isinstance(callback, TransitionCallback):
         await query.message.edit_reply_markup(reply_markup=callback.keyboard())
 
@@ -149,6 +148,7 @@ async def user_logout_init(msg: Message, state: FSMContext):
 @router.callback_query(StateFilter(DialogStates.ReportCommentAwaiting),
                        all_callbacks.FinalizeReport.filter(F.action == all_callbacks.Action.report))
 async def send_report_no_comment(query: CallbackQuery, state: FSMContext, reporter: Reporter):
+    await query.message.edit_text('Report is sent. Thank you!')
     user_data = await state.get_data()
     user_report = (ReportBuilder()
                    .add_category(user_data['report']['category'])
@@ -157,8 +157,6 @@ async def send_report_no_comment(query: CallbackQuery, state: FSMContext, report
                    .stamp_datetime()
                    ).build()
     reporter.report(user_report)
-
-    await query.message.edit_text('Report is sent. Thank you!')
     await state.set_state(DialogStates.Authorized)
 
 
@@ -168,8 +166,6 @@ async def extend_report_with_comment(msg: Message, state: FSMContext, reporter: 
     user_data = await state.get_data()
     report = user_data["report"]
     report["description"] = description
-
-    await state.update_data(report=report)
 
     user_report = (ReportBuilder()
                    .add_category(report['category'])
@@ -182,6 +178,7 @@ async def extend_report_with_comment(msg: Message, state: FSMContext, reporter: 
 
     await msg.answer('Report is sent! Thank you for detailed failure description!')
     await state.set_state(DialogStates.Authorized)
+    await state.update_data(report=report)
 
 
 @router.callback_query(StateFilter(DialogStates.ReportCommentAwaiting),
@@ -196,4 +193,3 @@ async def report_processing(
     callback = report_provider.get_callback(go_to)
     await query.message.edit_text(text='Report a failure!', reply_markup=callback.keyboard())
     await state.set_state(DialogStates.Reporting)
-
