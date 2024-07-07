4a4b5d9a5d343aa2dc396fe857d1d8e66c7d476c
Artur <arturmuxutdinov@gmail.com>
2024-07-07 16:21:43 +0300

    impl(api-server): cors

---
+++ src/main/java/com/java/configuration/WebConfig.java
@@ -0,0 +1,16 @@
+package com.java.configuration;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
