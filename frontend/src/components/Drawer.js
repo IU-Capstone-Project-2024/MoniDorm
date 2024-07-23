d8036b308dd9c3f7aac1ae2804c5c6c80e50afcf
dprostiruk <67105855+dprostiruk@users.noreply.github.com>
2024-07-23 20:27:38 +0300

    fix Grafana kaif

--- frontend/src/components/Drawer.js
+++ frontend/src/components/Drawer.js
@@ -10,7 +10,8 @@ const Drawer = () => {
   };
 
   const generateIframeSrc = (panelId) => {
-    return `http://10.90.137.18:3000/d-solo/fdpa1canapou8a/monidorm-dashboard?orgId=1&from=1721031065935&to=1721635865935&theme=light&panelId=${panelId}`;
+    let mid = Date.now()
+    return `http://10.90.137.18:3000/d-solo/fdpa1canapou8a/monidorm-dashboard?orgId=1&from=${mid - 4500000}&to=${mid + 300000}&theme=light&panelId=${panelId}`;
   };
 
   // Function to determine if the link is active based on the current location
